e3fd0633aa33a0e0fa88e809b58b2f53fed7db1c
Wanderley <100Palavr@gmail.com>
2022-05-25 15:55:51 -0300

    Update component.hpp

--- component.hpp
+++ component.hpp
@@ -6,16 +6,26 @@
 #include <string>
 #include <algorithm>
 
+typedef sf::Vector2f Point;
+
 class component {
 public:
     sf::RenderWindow& window;
     sf::Rect<float>   limits;
-    std::string       text;
+    sf::Text          title;
+    sf::Font          font;
 
     component(sf::RenderWindow& wnd) : window(wnd) {
+        std::cout << "Fonte carregada" << std::endl;
+        if(!font.loadFromFile("LinLibertine_aBL.ttf")) {
+            std::cout << "Ocorreu algum erro no load da imagem" << std::endl;
+        }
 
+        title.setFont(font);
+        title.setScale(Point(0.5, 0.5));
     }
 
+    virtual void set_text(std::string title) = 0;
     virtual void draw()      = 0;
     virtual bool collision() = 0;
     virtual void on_click() {};
