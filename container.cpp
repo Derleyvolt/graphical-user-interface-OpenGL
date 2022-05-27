0c6a93b94c7897abfd6dd752ea3dfbc510fa7b2e
Wanderley <100Palavr@gmail.com>
2022-05-27 11:09:45 -0300

    Update container.cpp

--- container.cpp
+++ container.cpp
@@ -1,6 +1,6 @@
 #include "container.hpp"
 
-container::container(sf::RenderWindow& wnd, int h, int w, Point pos, sf::Color cl) : component(wnd) {
+container::container(sf::RenderWindow& wnd, sf::Event& ev, int h, int w, Point pos, sf::Color cl) : component(wnd, ev) {
     // limites do drag-bar
     drag_bar_limits = sf::Rect<float>(pos, Point(w, DRAG_BAR_HEIGHT));
 
@@ -23,6 +23,11 @@ container::container(sf::RenderWindow& wnd, int h, int w, Point pos, sf::Color c
     rectangle.setSize(Point(w, h));
     rectangle.setPosition(pos);
     rectangle.setFillColor(cl);
+
+
+    // configuração dos limites da área que vai conter os componentes
+
+
 }
 
 void container::set_text(std::string text) {
@@ -37,7 +42,33 @@ void container::draw() {
 }
 
 bool container::collision() {
-    return 1;
+    sf::Vector2i mouse_pos = mouse.getPosition(window);
+    if(this->drag_bar_limits.contains(Point(mouse_pos.x, mouse_pos.y))) {
+        if(mouse.sf::Mouse::isButtonPressed(sf::Mouse::Left) && this->is_pressed == false) {
+            this->is_pressed = true;
+            this->last_click = Point(mouse_pos.x, mouse_pos.y);
+        }
+    }
+
+    if(mouse.sf::Mouse::isButtonPressed(sf::Mouse::Left) == false) {
+        this->is_pressed = false;
+    }
+
+    if(this->is_pressed) {
+        move(Point(mouse_pos.x, mouse_pos.y));
+    }
+}
+
+void container::move(Point pos) {
+    Point drag_offset(last_click.x-drag_bar.getPosition().x, last_click.y-drag_bar.getPosition().y);
+    Point drag_offset(last_click.x-drag_bar.getPosition().x, last_click.y-drag_bar.getPosition().y);
+
+    this->drag_bar.setPosition(Point(pos.x ))
+
+    drag_bar;
+    drag_bar_limits;
+
+    draw_limits;
 }
 
 Point container::get_title_position(const std::string arg) const {
