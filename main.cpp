71996231607ef1c09cce65ce996c1362dc062195
Wanderley <100Palavr@gmail.com>
2022-05-27 11:10:29 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,17 +4,19 @@
 #include <memory>
 #include <algorithm>
 #include "container.hpp"
-//#include <SFML/Graphics.hpp>
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    component* menu = new container(window, 300, 200, Point(60, 80));
+    sf::Event event;
+
+    component* menu = new container(window, event, 300, 200, Point(60, 80));
+
+    menu->set_text("Algorithms");
 
     while (window.isOpen()) {
-        sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
