31f1d27fd3bf97272271163bd418314ed944ac33
Wanderley <100Palavr@gmail.com>
2022-05-27 11:09:29 -0300

    Update container.hpp

--- container.hpp
+++ container.hpp
@@ -10,13 +10,15 @@
 
 #define DRAG_BAR_HEIGHT            20
 #define UBUNTU_LIGHT_OFFSET        7
+#define DRAWABLE_FIELD
 
 class container : public component {
 public:
-    container(sf::RenderWindow& wnd, int h = 300, int w = 200, Point pos = Point(0, 0), 
+    container(sf::RenderWindow& wnd, sf::Event& ev, int h = 300, int w = 200, Point pos = Point(0, 0), 
               sf::Color cl = sf::Color(46, 52, 64, 220));
 
     void set_text(std::string text);
+    void move(Point pos);
 
     void draw();
 
@@ -31,6 +33,10 @@ private:
     // drag_bar
     sf::RectangleShape drag_bar;
     sf::Rect<float>    drag_bar_limits;
+
+    // limite relacionado à area onde os componentes vão ficar
+    sf::Rect<float>    draw_limits;
+    sf::Point          last_click;
 };
 
 #endif
