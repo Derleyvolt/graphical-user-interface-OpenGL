1e9a28055e5b462d6889085377173551b21fc6a8
Wanderley <100Palavr@gmail.com>
2022-05-27 11:10:03 -0300

    Update component.hpp

--- component.hpp
+++ component.hpp
@@ -2,6 +2,7 @@
 #define BOUND
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 #include <vector>
 #include <string>
 #include <algorithm>
@@ -13,15 +14,20 @@ private:
     sf::Font font;
 
 public:
+    bool is_pressed;
+
     sf::RenderWindow& window;
     sf::Rect<float>   limits;
     sf::Text          title;
+    sf::Event         event;
+    sf::Mouse         mouse;
 
-    component(sf::RenderWindow& wnd);
+    component(sf::RenderWindow& wnd, sf::Event& ev);
 
     virtual void set_text(std::string title) = 0;
-    virtual void draw()      = 0;
-    virtual bool collision() = 0;
+    virtual void draw()           = 0;
+    virtual void move(Point pos)  = 0;
+    virtual bool collision()      = 0;
     virtual void on_click() {};
     virtual void on_hover() {};
 };
